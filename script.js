9adec78a30cb6442dd6e553487568b3ee07ee488
JohannesTB <56443549+JohannesTB@users.noreply.github.com>
2019-12-04 17:46:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function playSample(sample) {
+    var sound = new Audio("assets/" + sample);
+    sound.play();
+}
+function arrays() {
+    var samplenames = ["assets/kick.mp3", "assets/hihat.mp3", "assets/kick.mp3", "assets/hihat.mp3", "assets/kick.mp3", "assets/snare.mp3", "assets/snare.mp3", "assets/snare.mp3", "assets/kick.mp3", "assets/hihat.mp3", "assets/kick.mp3", "assets/hihat.mp3", "assets/kick.mp3", "assets/laugh2.mp3"];
+    var index = 0;
+    var track = setInterval(playTrack, 300);
+    function playTrack() {
+        var playAudio = new Audio(samplenames[index]);
+        playAudio.play();
+        index++;
+    }
+}
+window.addEventListener("load", function () {
+    document.querySelector("#kick").addEventListener("mousedown", function () { playSample("kick.mp3"); });
+    document.querySelector("#snare").addEventListener("mousedown", function () { playSample("snare.mp3"); });
+    document.querySelector("#hihat").addEventListener("mousedown", function () { playSample("hihat.mp3"); });
+    document.querySelector("#c").addEventListener("mousedown", function () { playSample("C.mp3"); });
+    document.querySelector("#f").addEventListener("mousedown", function () { playSample("F.mp3"); });
+    document.querySelector("#g").addEventListener("mousedown", function () { playSample("G.mp3"); });
+    document.querySelector("#a").addEventListener("mousedown", function () { playSample("A.mp3"); });
+    document.querySelector("#voc1").addEventListener("mousedown", function () { playSample("laugh1.mp3"); });
+    document.querySelector("#voc2").addEventListener("mousedown", function () { playSample("laugh2.mp3"); });
+    document.querySelector("#play").addEventListener("click", arrays);
+});
+//# sourceMappingURL=script.js.map
